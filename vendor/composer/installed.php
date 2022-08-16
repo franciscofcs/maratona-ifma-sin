246dd8f348e4346a87d2e917652a476273aa0a3f
Francisco da Conceição Silva <francisco.conceicao@ifma.edu.br>
2022-08-16 00:23:53 -0300

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c22309dfa3c18acad2c58ca59c36cac236e056b0',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c22309dfa3c18acad2c58ca59c36cac236e056b0',
+            'dev_requirement' => false,
+        ),
+    ),
+);
